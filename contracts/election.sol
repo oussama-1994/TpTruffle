601fd947d736b949d6de1053b0601795f6c9b993
oussama-1994 <oussama.slimani@tek-up.tn>
2019-10-12 16:31:34 +0100

    first commit

---
+++ contracts/election.sol
@@ -0,0 +1,59 @@
+pragma  solidity ^0.5.0;
+
+contract Election{
+    
+    struct Candidate {
+
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    
+    mapping(uint => Candidate) public candidates;
+ 
+    uint public candidatesCount;
+    event votedEvent (
+        uint indexed _candidateId
+    );
+    constructor() public {
+
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        addCandidate("Candidate 3");
+        addCandidate("Candidate 4");
+    }
+
+    function addCandidate (string memory _name) private {
+
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+    
+
+
+
+
+}
